65a0ef9e79291320ea14165c559f1be55b9977c9
AndreaProzzo21 <and.prozzo@gmail.com>
2026-05-18 22:18:07 +0200

    Stable entry point

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,13 @@
 # main.py
 import uvicorn
-import os
-
-# Assicurati che l'import rispecchi la posizione di api_server.py
-# Se api_server è dentro app/api/ usiamo app.api.api_server
 from app.api.api_server import create_app
+from app.core.config import settings
 
 app = create_app()
 
 if __name__ == "__main__":
-    # Leggiamo porta e host da env per flessibilità (opzionale)
-    port = int(os.getenv("GATEWAY_PORT", 8000))
-    
     uvicorn.run(
         "main:app", 
-        host="0.0.0.0", 
-        port=port, 
-        reload=True
+        host=settings.HOST, 
+        port=settings.PORT
     )
